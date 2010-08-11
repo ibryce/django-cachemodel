953985da361ac20c4f4e804b827fb67b13ef2275
Wiggins <wiggins@concentricsky.com>
2010-08-11 13:30:50 -0700

    setuptoolsd

---
+++ setup.py
@@ -0,0 +1,13 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='django-cachemodel',
+    version='0.9.0',
+    packages = find_packages(),
+
+    author = 'Concentric Sky',
+    author_email = 'django@concentricsky.com',
+    description = 'Concentric Sky\'s cachemodel library',
+    license = 'Apache2'
+)
