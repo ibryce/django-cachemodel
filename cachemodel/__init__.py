b1f795ef07eff33ff57cd683fcab0ec968c251df
Charlie <clamothe@Charlies-MacBook-Pro.local>
2011-07-15 09:13:29 -0700

    Allow non-ascii cache key names.

--- cachemodel/__init__.py
+++ cachemodel/__init__.py
@@ -11,7 +11,7 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
-VERSION = (0,9,7)
+VERSION = (0,9,8)
 
 CACHE_TIMEOUT = None
 
