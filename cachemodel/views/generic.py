b73482b772ab9282077ca34b2eeba262f9ebb540
M. Wiggins <code@hlidskialf.com>
2011-09-24 00:11:01 -0700

    cleanup generic CachedDetailView

--- cachemodel/views/generic.py
+++ cachemodel/views/generic.py
@@ -2,16 +2,14 @@ from cachemodel.models import CacheModel
 from django.views.generic import DetailView
 
 class CachedDetailView(DetailView):
-    def get_object(self, **kwargs):
-        if not isinstance(self.model, CacheModel):
-            return super(CachedDetailView, self).get_object(**kwargs)
-        pk = self.kwargs.get('pk', None)
-        slug = self.kwargs.get('slug', None)
-        if pk is not None:
-            obj = self.model.objects.get_by_pk(pk)
-        elif slug is not None:
-            obj = self.model.objects.get_by_slug(slug)
-        else:
-            raise AttributeError('Need either pk or slug')
-        return obj
+    def get_object(self, *args, **kwargs):
+        get_by_fun = getattr(self.model.objects, 'get_by', None)
+        if not callable(get_by_fun):
+            return super(CachedDetailView, self).get_object(*args, **kwargs)
 
+        for arg in ('pk','slug'):
+            value = self.kwargs.get(arg, None)
+            if value is not None:
+                return get_by_fun(arg, value)
+
+        raise AttributeError('Need either pk or slug')
